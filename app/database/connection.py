a1cc71dc6be2abdd12af6294764d328429ae35ec
Mars666 <chengshiang335@gmail.com>
2026-05-29 11:30:38 +0800

    .env + config 分離（production 標準）

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,13 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#DATABASE_URL = "postgresql://postgres:postgres@localhost/fastapi_db"
-DATABASE_URL = (
-    "postgresql://postgres:postgres@db:5432/fastapi_db"
-)
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
